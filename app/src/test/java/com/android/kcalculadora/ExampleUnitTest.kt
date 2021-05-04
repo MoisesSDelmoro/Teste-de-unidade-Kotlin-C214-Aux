bea7d0bab576ecaa0fdf4a34ae2a81e29adeefd7
MoisesSDelmoro <57488202+MoisesSDelmoro@users.noreply.github.com>
2021-05-04 17:27:28 -0300

    test Sub

--- app/src/test/java/com/android/kcalculadora/ExampleUnitTest.kt
+++ app/src/test/java/com/android/kcalculadora/ExampleUnitTest.kt
@@ -14,4 +14,8 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun subtraction_noCorrect(){
+        assertNotEquals(6,10-5)
+    }
 }
